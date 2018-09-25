5c6a0f4d6d72cd4eb3287f66591956c5c5b22930
vipulrawat <vipulrawat.imi@live.in>
2018-09-25 17:02:04 +0530

    [WIP]

--- question-13/script.js
+++ question-13/script.js
@@ -1,12 +1,17 @@
 // eslint-disable-next-line
+const btn = document.getElementById('btn');
+btn.style.display = 'none';
+// eslint-disable-next-line
 window.onscroll = function () {
   // eslint-disable-next-line
-  if (document.body.scrollTop > 400) {
+  if ( document.documentElement.scrollTop > 1000) {     // [*]
     // eslint-disable-next-line
-    document.getElementById("btn").style.display = "block";
+    btn.style.display = "block";
+    // console.log('scrooled 100')
   } else {
     // eslint-disable-next-line
     document.getElementById("btn").style.display = "none";
+    // console.log('not scrolled');
   }
 };
 // eslint-disable-next-line
